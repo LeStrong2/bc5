ad22c55c0a8c7800d582c9be1f7f08651b463d6d
LeStrong2 <53224657+LeStrong2@users.noreply.github.com>
2019-09-11 00:42:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,5 +37,28 @@ if (message.content.startsWith(adminprefix + 'setava')) {
 }
 });
 
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+        msg.channel.bulkDelete(1000).then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
 
 client.login(process.env.TOKEN);
