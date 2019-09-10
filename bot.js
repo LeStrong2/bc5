a91d9e4310d184c429f2104e5e341f13d9ba9406
LeStrong2 <53224657+LeStrong2@users.noreply.github.com>
2019-09-11 00:55:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,11 +42,7 @@ client.on("message", message => {
  
             var args = message.content.substring(prefix.length).split(" ");
             if (message.content.startsWith(prefix + "clear")) {
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
-        var msg;
-        msg = parseInt();
-      
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
+      message.channel.fetchMessages({limit: msg}).then(messages => message.DM.bulkDelete(messages)).catch(console.error);
       message.channel.sendMessage("", {embed: {
         title: "Done | تــم مسح الشات",
         color: 0x06DF00,
