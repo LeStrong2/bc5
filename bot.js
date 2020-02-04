1a515ee35b0010b480916aec2cdb52368caaa674
LeStrong2 <53224657+LeStrong2@users.noreply.github.com>
2020-02-04 14:09:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,52 @@ var prefix = "#"
 var adminprefix = '#'
 
 
+
+
+//bc
+
+client.on("message", message => {
+    if (message.content.startsWith("#bc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
+
+
+client.on('ready',  () => {
+    console.log('تم تشغيل :Broadcast  ');
+    console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
+    console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
+  });
+
+
+  client.on('message', msg => {
+    if(msg.content === '#help')
+    msg.reply('Check Your DM :white_check_mark:')
+  });
   
+  
+  client.on("message", message => {
+    if (message.content === "#help") {
+     const embed = new Discord.RichEmbed() 
+         .setColor("#00FF00")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`**Help|هيلب
+
+       #bc | لأرسال برود كاست للكل
+
+       ** `)
+   message.author.sendEmbed(embed)
+   
+   }
+   });
 
 const developers = ["560360141749354496","id"]
 client.on('message', message => {
@@ -13,7 +58,10 @@ client.on('message', message => {
       
   if (message.content.startsWith(adminprefix + 'setg')) {
     client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)       
+      message.channel.send(`**✅   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'setw')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
